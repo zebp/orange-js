6df62d3a04331b29305be3c5b264244a4d0a9c9c
Zeb Piasecki <zeb@zebulon.dev>
2025-02-02 10:33:57 -0500

    feat: bundle isolation

---
+++ packages/vite/src/plugins/isolation.ts
@@ -0,0 +1,38 @@
+/*
+  Allows for creating modules that are isolated from client or server bundles by using
+  a `.client` or `.server` suffix.
+ */
+import { Plugin } from "vite";
+import { Context } from "../index.js";
+
+export function isolation(ctx: Context): Plugin[] {
+  const clientRegex = /.+.client(?:.(?:j|t)sx?)?$/g;
+  const serverRegex = /.+.server(?:.(?:j|t)sx?)?$/g;
+
+  return [
+    {
+      name: "orange:client-isolation",
+      enforce: "pre",
+      resolveId(source, importer) {
+        // Only apply this plugin if the client manifest is available, (ie. we're building the server bundle)
+        if (ctx.clientManifest !== undefined && clientRegex.test(source)) {
+          throw new Error(
+            `${source} was imported in the server bundle, but is a client-only module. Importer: ${importer}`,
+          );
+        }
+      },
+    },
+    {
+      name: "orange:server-isolation",
+      enforce: "pre",
+      resolveId(source, importer) {
+        // Only apply this plugin if the client manifest isn't available, (ie. we're building the client bundle)
+        if (ctx.clientManifest === undefined && serverRegex.test(source)) {
+          throw new Error(
+            `${source} was imported in the client bundle, but is a server-only module. Importer: ${importer}`,
+          );
+        }
+      },
+    },
+  ];
+}
