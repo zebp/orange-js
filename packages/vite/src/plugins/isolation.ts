39a4e2c1fff72dce5029eef6c9bc98b74894f663
Zeb Piasecki <zeb@zebulon.dev>
2025-02-09 09:04:22 -0500

    fix: improve isolation

--- packages/vite/src/plugins/isolation.ts
+++ packages/vite/src/plugins/isolation.ts
@@ -3,7 +3,7 @@
   a `.client` or `.server` suffix.
  */
 import { Plugin } from "vite";
-import { Context } from "../index.js";
+import { VirtualModule } from "../virtual-module.js";
 
 export function isolation(): Plugin[] {
   const clientRegex = /.+.client(?:.(?:j|t)sx?)?$/g;
@@ -17,14 +17,24 @@ export function isolation(): Plugin[] {
       applyToEnvironment(environment) {
         return environment.name !== "client";
       },
-      resolveId(source, importer) {
+      async resolveId(source, importer) {
         // TODO: this crashes the dev server
         if (importer?.endsWith(".html")) return;
 
         if (clientRegex.test(source)) {
-          this.error(
-            `${source} was imported in the server bundle, but is a client-only module. Importer: ${importer}`,
-          );
+          const resolved = await this.resolve(source, importer);
+          if (!resolved) return;
+
+          this.debug(`Client-only module ${source} (${resolved.id}) is being isolated`);
+
+          return new VirtualModule(`isolated/${resolved.id}`).id;
+        }
+      },
+      async load(id) {
+        const maybeId = VirtualModule.findPrefix("isolated/", id);
+        if (maybeId) {
+          const code = await this.load({ id: maybeId });
+          return emptyExports(code.exports ?? []);
         }
       },
     },
@@ -35,16 +45,36 @@ export function isolation(): Plugin[] {
       applyToEnvironment(environment) {
         return environment.name === "client";
       },
-      resolveId(source, importer) {
+      async resolveId(source, importer) {
         // TODO: this crashes the dev server
         if (importer?.endsWith(".html")) return;
 
         if (serverRegex.test(source)) {
-          this.error(
-            `${source} was imported in the client bundle, but is a server-only module. Importer: ${importer}`,
-          );
+          const resolved = await this.resolve(source, importer);
+          if (!resolved) return;
+
+          this.debug(`Server-only module ${source} (${resolved.id}) is being isolated`);
+
+          return new VirtualModule(`isolated/${resolved.id}`).id;
+        }
+      },
+      async load(id) {
+        const maybeId = VirtualModule.findPrefix("isolated/", id);
+        if (maybeId) {
+          const code = await this.load({ id: maybeId });
+          return emptyExports(code.exports ?? []);
         }
       },
     },
   ];
 }
+
+const emptyExports = (exports: string[]) => {
+  return exports
+    .map((e) =>
+      e === "default"
+        ? "export default undefined;"
+        : `export const ${e} = undefined;`
+    )
+    .join("\n");
+};
